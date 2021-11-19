e22d67529921ea1e2fdad26bef246040222277ea
LaurePiechaczyk <laure.piecha@gmail.com>
2021-11-19 14:49:24 +0000

    animations second part home page

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,16 +1,27 @@
 const CoffeeLiquide = document.querySelector('.coffe-pot img:nth-child(1)')
 const emptyCoffeePot = document.querySelector('.coffe-pot img:nth-child(2)')
-const introText = document.querySelector('.intro-text')
+const entireCoffeePot = document.querySelector('.coffe-pot')
 
+const coffeeCup = document.querySelector('.coffe-cup img:nth-child(1)')
+const coffeeSmoke = document.querySelector('.coffe-cup img:nth-child(2)')
+
+const introText = document.querySelector('.intro-text')
+const coffeeBag = document.querySelector('.coffe-bag-container')
+const coffeeLeaf = document.querySelector('.coffe-leaf-container')
+const coffeeFruits = document.querySelector('.coffe-fruits-container')
+
+const secondPartText = document.querySelector('.second-part-text')
+
+
+
+// --------------- Pot animations
 let potAnimationDuration = 3; 
 
 let potScrollTrigger  = {
     trigger: ".coffee-animation",
-    markers:true,
     start: "100px 200px",
-    markers:true,
     toggleActions: "play pause reverse reset",
-  
+    
     };
 
 gsap.to(emptyCoffeePot, {
@@ -29,9 +40,76 @@ gsap.to(CoffeeLiquide, {
    duration: potAnimationDuration ,
 })
 
-/*
+
+// --------------- Introtexte animation
+let introTextScrollTrigger  = {
+    trigger: ".coffee-beans-background",
+    start: "top 550px",
+    endTrigger: "footer",
+    end:"bottom top",
+    
+    toggleActions: "play none none reverse",
+    };
+
+
 gsap.to(introText, {
     opacity: 0,
-   scrollTrigger: potScrollTrigger,
+    scrollTrigger: introTextScrollTrigger,
+    duration: 1,
+    display:"none",
+})
+
+// --------------- coffebag animation
+gsap.to(coffeeBag, {
+    scrollTrigger: introTextScrollTrigger,
+    duration: 6,
+    y:0,
+})
+
+// --------------- coffee leaf animation
+let plantsTextScrollTrigger  = {
+    trigger: ".coffee-beans-background",
+    start: "top 0px",
+    endTrigger: "footer",
+    end:"bottom top",
+    markers:true,
+    toggleActions: "play none none reverse",
+    };
+
+gsap.to(coffeeLeaf, {
+    opacity: 1,
+    scrollTrigger: plantsTextScrollTrigger,
+    duration: 6,
+})
+
+// --------------- coffee fruits animation
+gsap.to(coffeeFruits, {
+    opacity: 1,
+    scrollTrigger: plantsTextScrollTrigger,
+    duration: 10,
+})
+
+// --------------- second-part-text animation
+gsap.to(secondPartText, {
+    opacity: 1,
+    scrollTrigger: plantsTextScrollTrigger,
+    duration: 5,
+})
+
+
+
+// --------------- Pot disapearance
+gsap.to(entireCoffeePot, {
+    opacity: 0,
+    scrollTrigger: plantsTextScrollTrigger,
+    duration: 2,
+})
+
+// --------------- cup animation
+/*
+gsap.to(coffeeCup, {
+    opacity: 1,
+    scrollTrigger: plantsTextScrollTrigger,
+    duration: 10,
 })
 */
