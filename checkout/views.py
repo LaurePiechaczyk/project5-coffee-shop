35de598a275dcaae55598af87c780d397ed63781
LaurePiechaczyk <laure.piecha@gmail.com>
2021-11-23 12:29:34 +0000

    checkout form automatically filled with profile data

--- checkout/views.py
+++ checkout/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
 from .forms import OrderForm
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
 
 
 def checkout(request):
@@ -11,7 +13,26 @@ def checkout(request):
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
-    order_form = OrderForm()
+    if request.user.is_authenticated:
+        try:
+            profile = UserProfile.objects.get(user=request.user)
+            order_form = OrderForm(initial={
+                'full_name': profile.user.get_full_name(),
+                'email': profile.user.email,
+                'phone_number': profile.default_phone_number,
+                'country': profile.default_country,
+                'postcode': profile.default_postcode,
+                'town_or_city': profile.default_town_or_city,
+                'street_address1': profile.default_street_address1,
+                'street_address2': profile.default_street_address2,
+                
+            })
+        except UserProfile.DoesNotExist:
+            order_form = OrderForm()
+    else:
+        order_form = OrderForm()
+
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
