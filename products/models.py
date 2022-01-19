6e5123c61db4d44f7fd7f3ed072202d8c32ec407
LaurePiechaczyk <laure.piecha@gmail.com>
2022-01-19 18:26:18 +0000

    strongness back in charfield

--- products/models.py
+++ products/models.py
@@ -32,7 +32,7 @@ class Strongness(models.Model):
     class Meta:
         verbose_name_plural = 'Strongness'
         
-    name = models.DecimalField(max_digits=1, decimal_places=0)
+    name = models.CharField(max_length=254)
 
     def __str__(self):
         return self.name
